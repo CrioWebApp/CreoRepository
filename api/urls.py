3cfac5bcc3236886be3597cf04d00209f21487dd
c_Door in <egor1@mail.ru>
2024-10-09 20:02:11 +0300

    Change token create URI

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, re_path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from api.views import DataValidation
+from djoser.views import TokenCreateView, TokenDestroyView
 
 urlpatterns = [
     path('dataValidation', DataValidation.as_view(), name='Action'),
-    re_path(r'^apiAuth/', include('djoser.urls.authtoken')),
+    re_path(r'^apiAuth/token/?$', TokenCreateView.as_view(), name="login"),
+    re_path(r'^apiAuth/token/logout/?$', TokenDestroyView.as_view(), name="logout"),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
